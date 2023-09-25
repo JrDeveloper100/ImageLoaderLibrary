af6fc709e15beac2a4fd9b5824bbfe67c8f6cc5e
JrAppDeveloper <jrappdeveloper.cdg@gmail.com>
2023-09-25 16:12:15 +0500

    some changes

--- ImageLoader/src/main/java/com/example/imageloader/ImageLoader.kt
+++ ImageLoader/src/main/java/com/example/imageloader/ImageLoader.kt
@@ -1,13 +1,18 @@
 package com.example.imageloader
 
 import android.content.Context
+import android.net.Uri
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 
 object ImageLoader {
-    fun load(context: Context, imageUrl: String, imageView: ImageView){
+    fun load(context: Context, imageUri: Uri, imageView: ImageView){
         Glide.with(context)
-            .load(imageUrl)
+            .load(imageUri)
+            .error(R.drawable.placeholder)
+            .placeholder(R.drawable.placeholder)
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
             .into(imageView)
 
     }
--- app/src/main/java/com/example/imageloaderlibrary/MainActivity.kt
+++ app/src/main/java/com/example/imageloaderlibrary/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.example.imageloaderlibrary
 
+import android.content.ContentValues
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
+import com.example.imageloader.ImageLoader
 
 class MainActivity : AppCompatActivity() {
+    private val REQUEST_IMAGE_SELECTION = 1
+    private lateinit var imageView: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        imageView= findViewById(R.id.imageView)
+        val btnSelectImage: Button = findViewById(R.id.btnSelectImage)
+        btnSelectImage.setOnClickListener {
+            openGallery()
+        }
     }
+
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent, REQUEST_IMAGE_SELECTION)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_IMAGE_SELECTION && resultCode == RESULT_OK && data != null){
+            val imageUri: Uri? = data.data
+            if (imageUri != null){
+                ImageLoader.load(this,imageUri,imageView)
+            }
+        }
+    }
+
 }
